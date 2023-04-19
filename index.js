161bfcff8a9acba813152281142d4c667fb4f657
unknown <you@example.com>
2023-04-19 15:13:34 +0530

    wejhceww

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require("express")
+const app=express()
+app.use(express.json())
+const cors=require("cors")
+app.use(cors())
+const port =process.env.PORT || 4000
+
+app.get("/",(req,res)=>{
+    res.status(200).json("hello")
+})
+
+
+app.listen(port,()=>{
+    console.log("server started")
+})
